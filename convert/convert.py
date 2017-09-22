485d7ef54901bc52f45629e7ebc9a76c283b1e60
fvadzim <fvadzim@gmail.com>
2017-09-22 20:34:40 +0300

    seems to me the structure is fine

--- convert/convert.py
+++ convert/convert.py
@@ -4,6 +4,7 @@ import os
 import mysql.connector
 import sys
 import psycopg2
+import psycopg2.extras
 from interface import implements, Interface
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from  parse import *
@@ -22,57 +23,82 @@ class SqlConnection():
         self, host,
         user, password,
         database, charset=None):
-        self.connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database)
+        pass
 
     def get_table_fields(self, table_name):
-        cursor = self.connection.cursor()
-        cursor.execute("Select * FROM %s where id = 0" % (table_name,))
-        return [desc[0] for desc in cursor.description]
+        self.cursor.execute("Select * FROM %s where id = 0" % (table_name,))
+        return [desc[0] for desc in self.cursor.description]
 
-    def get_table_length(connection, table_name):
-        cursor = connection.cursor()
+    def get_table_length(self, table_name):
+        cursor = self.connection.cursor(buffered = True)
         cursor.execute("SELECT COUNT(*) from %s" % (table_name,));
         return cursor.fetchone()[0]
 
-class MySqlConnection(SqlConnection, implements(ISqlRawGetter)):
-    def __init__(self, host, user, password, database):
-        super().__init__(
-            host = host, user = user,
-            password = password, database = database)
-
     def get_table_raw(self, table_name, i, cursor = None):
-        if cursor == None:
+        if cursor is None:
             cursor = self.get_cursor()
         cursor.execute('SELECT * FROM %s where id= %d' % (table_name, i))
         try:
             fetched = cursor.fetchall()
-            fetched  = fetched[0]
+            fetched = fetched[0]
             return fetched
         except:
             print (i)
             print (fetched)
 
-    def get_cursor(self, **kwargs):
-        print("kwargs : ", kwargs)
-        if (len(kwargs) == 0):
-            return self.connection.cursor(dictionary=True,
-                              buffered=False)
-        else:
-            return self.connection.cursor(**kwargs)
-
-    def select_text_by_id(self, table_name, i, key, cursor = None):
-        if cursor == None:
-            cursor = self.get_cursor()
+    def select_field_by_id(self, table_name, i, key):
         try:
-            return get_table_raw[key]
+            return self.get_table_raw(
+                table_name=table_name, i=i)[key]
         except:
             return ''
 
-def json2csv():
+class _MySqlConnection(SqlConnection, implements(ISqlRawGetter)):
+    def __init__(self, host, user, password, database):
+        super().__init__(
+            host = host, user = user,
+            password = password, database = database)
+
+        self.connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database)
+        self.cursor = self.connection.cursor(buffered=True, dictionary=True)
+
+    def get_cursor(self, **kwargs):
+        print("kwargs : ", kwargs)
+        if (len(kwargs) == 0):
+            return self.connection.cursor(
+                dictionary=True,
+                buffered=False)
+        else:
+            return self.connection.cursor(**kwargs)
+
+class PostgresSqlConnecton(SqlConnection, implements(ISqlRawGetter)):
+    def __init__(self, host, user, password, database):
+        super().__init__(
+            host=host, user=user,
+            password=password, database=database)
+
+        self.connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database)
+        self.cursor = self.connection.cursor(
+            cursor_factory=psycopg2.extras.RealDictCursor)
+
+    def get_cursor(self, **kwargs):
+        print("kwargs : ", kwargs)
+        if (len(kwargs) == 0):
+            return self.connection.cursor(
+                cursor_factory=psycopg2.extras.RealDictCursor)
+        else:
+            return self.connection.cursor(**kwargs)
+
+
+'''def json2csv():
     in_file = open(file_path, 'r')
     if not out_file_path:
         out_file_path = os.path.join(
@@ -89,35 +115,37 @@ def json2csv():
                 delimiter='\t')
         break
     for row in json_file:
-        csv_writer.writerow(row)
+        csv_writer.writerow(row)'''
 
 def sql2csv(
-        host, user, password,
-        database, table_name, out_file_path):
-
-        sql_converter  = MySqlConnection(
+    host, user, password,
+    database, table_name, out_file_path):
+        sql_converter  = _MySqlConnection(
             host=host,
             user=user,
             password=password,
             database=database)
         print("TABLE NAME :",table_name, "len", len(table_name))
-
         csv_writer = csv.DictWriter(
                 open(out_file_path, 'w'),
                  fieldnames =  sql_converter.get_table_fields(table_name),
                 delimiter='\t')
-        cursor = sql_converter.get_cursor(
+        '''cursor = sql_converter.get_cursor(
             buffered=False,
-            dictionary=True)
+            dictionary=True)'''
         csv_writer.writeheader()
-        for i in range(1, sql_converter.get_table_length(table_name)
+        for i in range(1, sql_converter.get_table_length(table_name)):
+            raw = sql_converter.get_table_raw(table_name, i)
+            if raw:
+                # print (i)
+                csv_writer.writerow(raw)
 
-                           ):
-
-                        raw = sql_converter.get_table_raw(table_name, i, cursor)
-                        if raw:
-                            print (i)
-                            csv_writer.writerow(raw)
+def get_new_cursor(self, **kwargs):
+    print("kwargs : ", kwargs)
+    if not len(kwargs):
+        return self.cursor(dictionary=True, buffered=False)
+    else:
+        return self.cursor(**kwargs)
 
 if __name__=='__main__':
     #json_to_csv('./data/tonality/test.json')
@@ -130,7 +158,18 @@ if __name__=='__main__':
     sql2csv(
         host="localhost",
         user="root",
-        password="root",
-        database="bel_sites",
-        table_name='publications',
-        out_file_path='publications.csv')
+        password="ASSANGE",
+        database="pscraper",
+        table_name="publications",
+        out_file_path='contents.csv')
+    '''
+    lol = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="ASSANGE",
+        database="pscraper")
+
+    a = get_new_cursor(lol,
+        buffered=False,
+        dictionary=True)
+    print(a)'''
